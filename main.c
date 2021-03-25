bbb16f4f19e79b75dc40f75f8b63a025068c34b5
hedibousbih <Mohamedhedi.bousbih@esprit.tn>
2021-03-25 13:56:03 +0100

    mise a jour main

--- main.c
+++ main.c
@@ -4,9 +4,21 @@
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_ttf.h>
 #include "mini.h"
+#include "collision.h"
 
 int main(int argc, const char* argv[]) 
 {
+int x,y;
+
+SDL_Surface* source; SDL_Surface* destination;
+
+SDL_Surface *bg=IMG_Load( "backs.png" );
+
+SDL_Surface *ecran;
+
+ecran = SDL_SetVideoMode(720,400, 32, SDL_HWSURFACE);
+
+SDL_WM_SetCaption("lords of imposters", NULL);
 mini s;
 SDL_Event event;
 personnage p;
@@ -22,7 +34,7 @@ init_positions(&s);
     {   
 	Deplacement_personnage(&event,&p,&continuer,&s);   
 	init_affich(&s);
-	SDL_Flip(s.ecran);
+	SDL_Flip(ecran);
     }
 
 SDL_Quit();
